a7d137b8c5828216b8a1b600f564ad1648d54572
Saico <enrique.saico@ucsm.edu.pe>
2026-04-07 12:45:10 -0500

    feat: Se implemento el filtro por categoria en el catalogo

--- tienda-virtual/src/pages/Products.jsx
+++ tienda-virtual/src/pages/Products.jsx
@@ -1,14 +1,43 @@
+import { useState } from 'react'
 import products from '../data/products'
 import ProductCard from '../components/ProductCard.jsx'
 
 function Products() {
+  const [categoryFilter, setCategoryFilter] = useState('') // Estado para el filtro
+
+  // Filtramos los productos segun la categoria seleccionanda
+  const filteredProducts = categoryFilter
+	? products.filter((product) => product.category === categoryFilter)
+	: products
+
+  //OBtener todas las categorias unicas para el filtro
+  const categories = [...new Set(products.map((product) => product.category))]
+
   return (
     <section>
       <h1>Catálogo de productos</h1>
       <p>Explora nuestros productos disponibles.</p>
 
+      {/* Filtro de categoira */}
+      <div calssNamme="filter-container">
+	  <label htmlFor="category-filter">Filtrar por categoria:</label>
+	  <select
+	  	id="category-filter"
+	  	value={categoryFilter}
+	  	onChange={(e) => setCategoryFilter(e.target.value)}
+	  >
+	  	<option value="">Todos</option>
+	  	{categories.map((category) => (
+			<option key={category} value={category}>
+				{category}
+			</option>
+		))}
+	  </select>
+      </div>
+
+
       <div className="products-grid">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
